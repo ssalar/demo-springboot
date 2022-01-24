97535d56d0e84d410f52cf32e69a43e34594cc5a
Salar <ssalar8998@gmail.com>
2022-01-24 14:34:04 -0600

    yoyo entity added

--- src/main/java/com/demo/models/Student.java
+++ src/main/java/com/demo/models/Student.java
@@ -12,12 +12,30 @@ import javax.validation.constraints.NotBlank;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import java.util.logging.Logger;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 /**
  *
  * @author ssalar
  */
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private int id;
 //    private String name;
 //    private String email;
